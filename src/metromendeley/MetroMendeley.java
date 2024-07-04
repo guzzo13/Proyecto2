f8baea42c9f5399f45a6d8ee618ebe98ee2292f1
guzzo13 <diego.guzzo@correo.unimet.edu.ve>
2024-07-04 18:02:49 -0400

    primer commit

---
+++ src/metromendeley/MetroMendeley.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package metromendeley;
+
+import static Clases.CargarTxt.leerArchivo;
+import java.io.IOException;
+
+/**
+ *
+ * @author guzzo
+ */
+public class MetroMendeley {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+
+    }
+    
+}
